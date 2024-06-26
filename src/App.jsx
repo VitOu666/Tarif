5c9d32a3edf5ed318f3fa12b7bb114304d169e43
VitOu666 <lecielvert777@gmail.com>
2024-06-26 14:14:33 +0100

    ll

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,49 @@
 import React from 'react';
 import './App.css';
 import Card from './components/Card';
-import data from './components/data.json'
 
-function App() {
+
+const App = () => {
+  const cards = [
+    {
+      id: 1,
+      price: 300,
+      speed: 10,
+      colour: "#2cc1d5"
+    },
+    {
+        id: 2,
+        price: 450,
+        speed: 50,
+        colour: "#24ae8d"
+      },
+      {
+        id: 3,
+        price: 550,
+        speed: 100,
+        colour: "#f46a5f"
+      },
+      {
+        id: 4,
+        price: 1000,
+        speed: 200,
+        colour: "#333a44"
+      }
+  ];
 
   return (
-    <div className='cardContainer'>
-      {data.map((item) => (
-<Card key={item.name} {...item} />
-))}
+    <div className="cardContainer">
+      {cards.map((card, index) => (
+        <Card
+          key={index}
+          colour={card.colour}
+          price={card.price}
+          speed={card.speed}
+          className={index === 2 ? 'specialCard' : ''}
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
 
 export default App
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,22 +2,26 @@ import React from "react";
 import '../components/card.scss';
 
 
-export default function Card (props) {
-    const {price, speed, colour} = props;
+const Card = ({ colour, price, speed, className }) => {
     return (
-        <div className="card" >
-                <div className="top">
-                <p style={{backgroundColor: colour}} className="cardTitle">Безлимитный {price}</p>
-                <p style={{backgroundColor: colour}} className="cardPrice"> 
-                    <span className="currency">руб</span>
-                    <div className="priceLine">
-                        <span className="price">{price}</span>
-                        <span className="perMonth">/мес</span>
-                    </div>
-                </p>
-                </div>
-                <p className="cardSpeed">до {speed} Мбит/сек</p>
-                <p className="cardInfo">Обьем включенного трафика не ограничен</p>
+    <div className={`cardPriceContainer ${className}`}>
+        <div style={{ backgroundColor: colour, backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3))' }}className="titleDiv">
+            <p>Безлимитный {price}</p>
+        </div>
+        <div style={{ backgroundColor: colour }} className="priceDiv">
+            <span className="currency">руб</span>
+            <div className="priceLine">
+            <span className="price">{price}</span> <span className="perMonth">/мес</span>
             </div>
-    )
-}
+        </div>
+        <div className="speedDiv">
+            <p>до {speed} Мбит/сек</p>
+        </div>
+        <div className="infoDiv">
+            <p>Обьем включенного трафика не ограничен</p>
+        </div>
+    </div>
+    );
+  };
+  
+  export default Card;
